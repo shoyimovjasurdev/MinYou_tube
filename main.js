66f1df5211c02cab3a1bbb323ba4961ee079207c
ShoyimovJasur <jasurdev8@gmail.com>
2022-07-14 19:02:26 +0500

    min Yutubr

---
+++ main.js
@@ -0,0 +1,102 @@
+//==============Render fuction======================
+
+function render(d) {
+    $(".video__lenta").html("")
+    let sanoq = 0
+    d.map((i) => {
+        let card = `
+            <div class="col-3" >
+               <iframe frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen width="100%" height="45%" class="" src="${i.src}" ></iframe>
+                <div><p>${i.title}</p></div>
+                <button class="btn btn_dalete" onclick="deleteFun(${sanoq})" style="float:left;color: rgb(143, 26, 26);s" >Delete</button>
+                <button onclick="editeFun(${sanoq})" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn" style="float:right;color: rgb(8, 89, 8); " >Edite</button><br><br>
+                <div><h3>${i.date}</h3></div>
+            </div>  
+        `
+        $(".video__lenta").append(card)
+        sanoq++
+    })
+}
+render(video)
+
+//==============Search==================
+$(".search").on("input", () => {
+    let value = $(".search").val()
+    let result = video.filter(item => {
+        return item.title.toLowerCase().includes(value.toLowerCase())
+    })
+    render(result)
+})
+
+//==========Categories fuction============
+$(".all").on("click", () => {
+    render(video)
+})
+
+$(".it").on("click", () => {
+    let it = video.filter(i => {
+        return i.categories.includes("it")
+    })
+    render(it)
+})
+
+$(".sport").on("click", () => {
+    let sport = video.filter(i => {
+        return i.categories.includes("sport")
+    })
+    render(sport)
+})
+
+$(".muzik").on("click", () => {
+    let muzik = video.filter(i => {
+        return i.categories.includes("muzik")
+    })
+    render(muzik)
+})
+
+//=======ADDelement function======== 
+$(".btn_submit").on("click", () => {
+    let newUrl = $(".inUrl").val()
+    let newTitle = $(".inTitle").val()
+    let newDate = $(".inDate").val()
+    let newCat = $(".inCat").val()
+    video.push({
+        src: newUrl,
+        title: newTitle,
+        date: newDate,
+        categories: newCat
+    })
+    render(video)
+})
+
+// ==============Delete fuction===========
+function deleteFun(sanoq) {
+    video.splice(sanoq, 1)
+    render(video)
+}
+
+//=================Edite fuction===========
+
+let imageSrc
+let currentEditedItemIndex = 0
+function editeFun(index) {
+    currentEditedItemIndex = index
+    imageSrc = video[currentEditedItemIndex].src
+    $(".eUrl").val(" ")
+    $(".eTitle").val(video[index].title)
+    $(".eDate").val(video[index].date)
+    $(".eCat").val(video[index].categories)
+}
+
+$(".edite_btn").on("click", () => {
+    video[currentEditedItemIndex].src = $(".eUrl").val()
+    video[currentEditedItemIndex].title = $(".eTitle").val()
+    video[currentEditedItemIndex].categories = $(".eCat").val()
+    video[currentEditedItemIndex].date = $(".eDate").val()
+    imageSrc = ""
+    $(".eCat").val("")
+    $(".eDate").val("")
+    $(".eTitle").val("")
+    render(video)
+    
+})
---
+++ video.js
@@ -0,0 +1,116 @@
+let video = [
+    {
+        "id": 0121,
+        "title": "Dasturlash o'zi nima? | Dasturchi kim? | Dasturlashning qanday turlari bor?",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/Hsm6WWncC0Q",
+        "categories": "it",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0221,
+        "title": "Doston Ergashev - To'yi bo'lyapti | Достон Эргашев - Туйи буляпти",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/ZoENRFKfJWw",
+        "categories": "muzik",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0521,
+        "title": "Cristiano Ronaldo Top 10 Impossible Goals ● Is He Human??",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/HvQ1WrN6AP4",
+        "categories": "sport",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0821,
+        "title": "КАК 35 ЛЕТНИЙ МЕССИ УНИЧТОЖИЛ ЧЕМПИОНОВ ЕВРОПЫ",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/e9OMg_5N6u8",
+        "categories": "sport",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0321,
+        "title": "Sardor Rasulov - Sevgilimni to'yigacha",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/oYlKHENebXw",
+        "categories": "muzik",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0421,
+        "title": "1 из триллиона футбольных моментов",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/HL8Er3cP2b4",
+        "categories": "sport",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0122,
+        "title": "Mittivines - Bu bollardan eng yengisi.",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/nU1R8i12vfA",
+        "categories": "muzik",
+        "width":"560",
+        "height":"315"
+    },
+    
+   
+    {
+        "id": 0321,
+        "title": "Sardor Rasulov - Sevgilimni to'yigacha",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/oYlKHENebXw",
+        "categories": "muzik",
+        "width":"560",
+        "height":"315"
+    },
+   
+   
+    {
+        "id": 0921,
+        "title": "Dasturlashni o'rganishda xorijiy tillar ya'ni ingliz tili, rus tili qanchalik ahamiyatli?",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/hP7K6HhTTXg",
+        "categories": "it",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0621,
+        "title": "Цикл for...of (Отличия от других циклов Javascript + использование с объектами).",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/jQUYTS8pjpA",
+        "categories": "it",
+        "width":"560",
+        "height":"315"
+    },
+    
+    {
+        "id": 0721,
+        "title": "JavaScript Number Methods | JavaScript Full Tutorial",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/DUV73ireecw",
+        "categories": "it",
+        "width":"560",
+        "height":"315"
+    },
+    {
+        "id": 0222,
+        "title": "Samadjon Ruzmetov - Yor-yor | Самаджон Рузметов - Ёр-ёр ",
+        "date": "2022-05-31 18:42:00",
+        "src": "https://www.youtube.com/embed/g0T8Cz26yBo",
+        "categories": "muzik",
+        "width":"560",
+        "height":"315"
+    },
+   
+]
